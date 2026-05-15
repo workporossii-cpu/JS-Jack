0e6d04985ad36fa0d1f8db62440fd592c5e2f433
workporossii-cpu <workporossii@gmail.com>
2026-05-15 22:21:58 +0300

    Create pool.ts

---
+++ src/db/pool.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+import { config } from '../config';
+
+export const pool = new Pool({
+  connectionString: config.databaseUrl,
+  ssl: { rejectUnauthorized: false },
+  connectionTimeoutMillis: 15000,
+  family: 4,
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
